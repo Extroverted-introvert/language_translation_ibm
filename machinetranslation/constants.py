5d2171cebe8bcbc770cf6e661bfd2a24fe25ebac
Parth Tripathi <36081090+Extroverted-introvert@users.noreply.github.com>
2021-08-14 09:14:40 +0530

    Update constants.py
    Removed exposed credentials, set to generic

--- machinetranslation/constants.py
+++ machinetranslation/constants.py
@@ -1,2 +1,2 @@
-API_KEY = 'wN9TDs4ZktlY0kfnA5C-OiaDn1yY1ZgqXtIvhxqkDqwX'
-API_URL = 'https://api.eu-gb.language-translator.watson.cloud.ibm.com/instances/70dc9916-894d-4cc7-9df1-7887613a468f'
+API_KEY = <Enter User API Key Here>
+API_URL = <Enter User API URL Here>
